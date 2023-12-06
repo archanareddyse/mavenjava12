e845ff749633292cac1c224e0ded653db4a81e24
archanareddyse <101937605+archanareddyse@users.noreply.github.com>
2023-12-06 15:17:03 +0530

    Update App.java

--- src/main/java/com/kmit/javemaven/App.java
+++ src/main/java/com/kmit/javemaven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to IT A" );
+        System.out.println( "Welcome to IT Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" );
     }
 }
